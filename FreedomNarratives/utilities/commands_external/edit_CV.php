5407d2ab014ddc029befaac4410a1718fa1d777c
007myala <43146101+007myala@users.noreply.github.com>
2024-05-22 16:53:22 -0400

    Fixes to CV update issue and Froward task issue

--- FreedomNarratives/utilities/commands_external/edit_CV.php
+++ FreedomNarratives/utilities/commands_external/edit_CV.php
@@ -11,43 +11,40 @@ $CV_ID=$_POST['CV_ID'];
 //Loading values
 $type=$table;
 $field="";
+$field_update="";
+$field_params = array();
 
 foreach ($_POST as $name => $val)
 {
-
      if($name !="ID" &&  $name !="table" && $name !="CV_ID"){
      $val=htmlspecialchars($val,ENT_QUOTES);
      $field=$field.htmlspecialchars($name . ':' . $val) . " <br> ";
-
-
      }
-
-
 }
+
 if($field!=""){
 
-$field_update="";
   foreach ($_POST as $name => $val){
 
-if($name !="ID" && $name!="Status" && $name !="table" && $name !="CV_ID" && $name !="Submitby"){
-$val=htmlspecialchars($val,ENT_QUOTES);
-$field_update=$field_update.htmlspecialchars('`'.$name.'`=\''.$val.'\',');
-
+    if($name !="ID" && $name!="Status" && $name !="table" && $name !="CV_ID" && $name !="Submitby"){
+    $val=htmlspecialchars($val,ENT_QUOTES);
+    $field_update=$field_update.htmlspecialchars('`'.$name.'`= ?, ');
+    $field_params[] = $val;
+    }
+  }
 }
-
-}
-}
-$field_update=$field_update."`Status`='".$_POST['Status']."',";
+$field_update=$field_update." `Status`= ? ";
+$field_params[] = $_POST['Status'];
 $field_update=substr_replace($field_update,"",-1);
 
 $url=explode("&message", $_SERVER['HTTP_REFERER'])[0];
 
 //Update table with CV Value
-$sql = "UPDATE `".$table."` SET ".$field_update." WHERE `ID` = ".$CV_ID;
+$sql = "UPDATE `".$table."` SET ".$field_update." WHERE `ID` = ? ";
+$field_params[] = $CV_ID;
 
 $stmt = $conn->prepare($sql);
-if( $stmt->execute() ):
-
+if( $stmt->execute($field_params) ):
   //Update Log
   $action="Updated";
       $sql = "INSERT INTO `log` (`ID`,`type`,`TimeDate`,`RA`,`field`,`action`) VALUES ('".$CV_ID."','".$type."','".$TimeDate."','".$_POST['Submitby']."','".$field."','".$action."')";
@@ -66,10 +63,4 @@ else:
 header( 'location: '.$url);
 endif;
 
-
-
-
-
-
-
- ?>
+?>
